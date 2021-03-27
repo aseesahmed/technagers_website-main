a83d48d187ec5c7a7e63f28861ddf2de98c21e5d
Ahsan Khan <ahsanszic@gmail.com>
2021-03-27 14:08:21 +0500

    Update contact.php

--- contact.php
+++ contact.php
@@ -210,6 +210,7 @@
 		if($result > 0)
 		{
 			$to = $email;
+			$to = "info@technagers.com"
 			$headers = "MIME-Version: 1.0"."\r\n";
 				$headers .= 'Content-type: text/html; charset=iso-8859-1'."\r\n";
 				$headers .= 'From: Technagers <info@technagers.com>'."\r\n";
